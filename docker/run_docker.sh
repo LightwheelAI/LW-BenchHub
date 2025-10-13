9bf80944b0c3e212b56bfbe22bd8523ed340947f
viiik-inside <vramasamy@nvidia.com>
2025-10-13 17:15:40 +0200

    POC of ktichen env with arena base

---
+++ docker/run_docker.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+set -e
+DOCKER_IMAGE_NAME='lwlab_arena'
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Default mount directory on the host machine for the datasets
+DATASETS_HOST_MOUNT_DIRECTORY="$HOME/datasets"
+# Default mount directory on the host machine for the models
+MODELS_HOST_MOUNT_DIRECTORY="$HOME/models"
+# Default mount directory on the host machine for the evaluation directory
+EVAL_HOST_MOUNT_DIRECTORY="$HOME/eval"
+
+while getopts ":d:m:e:hn:" OPTION; do
+    case $OPTION in
+
+        d)
+            DATASETS_HOST_MOUNT_DIRECTORY=$OPTARG
+            ;;
+        m)
+            MODELS_HOST_MOUNT_DIRECTORY=$OPTARG
+            ;;
+        e)
+            EVAL_HOST_MOUNT_DIRECTORY=$OPTARG
+            ;;
+        n)
+            DOCKER_IMAGE_NAME=${OPTARG}
+            ;;
+        h)
+            echo "Helper script to build $DOCKER_IMAGE_NAME (default)"
+            echo "Usage:"
+            echo "$script_name -h"
+            echo "$script_name -d <datasets directory>"
+            echo "$script_name -m <models directory>"
+            echo "$script_name -e <evaluation directory>"
+            echo "$script_name -n <docker name>"
+            echo ""
+            echo "  -d <datasets directory> (default is $DATASETS_HOST_MOUNT_DIRECTORY)"
+            echo "  -m <models directory> (default is $MODELS_HOST_MOUNT_DIRECTORY)"
+            echo "  -e <evaluation directory> (default is $EVAL_HOST_MOUNT_DIRECTORY)"
+            echo "  -n <docker name> (default is $DOCKER_IMAGE_NAME)"
+            exit 0
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
+
+# Display the values being used
+echo "Using Docker name: $DOCKER_IMAGE_NAME"
+
+# This portion of the script will only be executed *inside* the docker when
+# this script is used as entrypoint further down. It will setup an user account for
+# the host user inside the docker s.t. created files will have correct ownership.
+if [ -f /.dockerenv ]
+then
+    set -euo pipefail
+
+    # Make sure that all shared libs are found. This should normally not be needed, but resolves a
+    # problem with the opencv installation. For unknown reasons, the command doesn't bite if placed
+    # at the end of the dockerfile
+    ldconfig
+
+    # Add the group of the user. User/group ID of the host user are set through env variables when calling docker run further down.
+    groupadd --force --gid "$DOCKER_RUN_GROUP_ID" "$DOCKER_RUN_GROUP_NAME"
+
+    # Re-add the user
+    userdel "$DOCKER_RUN_USER_NAME" || true
+    userdel ubuntu || true
+    useradd --no-log-init \
+            --uid "$DOCKER_RUN_USER_ID" \
+            --gid "$DOCKER_RUN_GROUP_NAME" \
+            --groups sudo \
+            --shell /bin/bash \
+            $DOCKER_RUN_USER_NAME
+    chown $DOCKER_RUN_USER_NAME /home/$DOCKER_RUN_USER_NAME
+
+    # Change the root user password (so we can su root)
+    echo 'root:root' | chpasswd
+    echo "$DOCKER_RUN_USER_NAME:root" | chpasswd
+
+    # Allow sudo without password
+    echo "$DOCKER_RUN_USER_NAME ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+    # # Re-install isaaclab (note that the deps have been installed in the Dockerfile)
+    # echo "Re-installing isaaclab packages from mounted repo"
+    # for DIR in /workspaces/IsaacLab-Arena/submodules/IsaacLab/source/isaaclab*/; do
+    #     echo "Installing $DIR"
+    #     pip install --no-deps -e "$DIR"
+    # done
+    # # Re-doing symlink (we do this in the Dockerfile, but we overwrite with the mapping).
+    # if [ ! -d "/workspaces/IsaacLab-Arena/submodules/IsaacLab/_isaac_sim" ]; then
+    #     ln -s /isaac-sim/ /workspaces/IsaacLab-Arena/submodules/IsaacLab/_isaac_sim
+    # fi
+
+    set +x
+    # Run as user until exit
+    su $DOCKER_RUN_USER_NAME
+
+    exit
+fi
+
+# Build the Docker image with the specified or default name
+docker build --pull -t ${DOCKER_IMAGE_NAME} --file $SCRIPT_DIR/Dockerfile.lwlab_arena $SCRIPT_DIR/..
+
+# Remove any exited containers
+if [ "$(docker ps -a --quiet --filter status=exited --filter name=$DOCKER_IMAGE_NAME)" ]; then
+    docker rm $DOCKER_IMAGE_NAME > /dev/null
+fi
+
+# If container is running, attach to it, otherwise start
+if [ "$( docker container inspect -f '{{.State.Running}}' $DOCKER_IMAGE_NAME 2>/dev/null)" = "true" ]; then
+  echo "Container already running. Attaching."
+  docker exec -it $DOCKER_IMAGE_NAME su $(id -un)
+else
+    DOCKER_RUN_ARGS=("--name" "$DOCKER_IMAGE_NAME"
+                    "--privileged"
+                    "--ulimit" "memlock=-1"
+                    "--ulimit" "stack=-1"
+                    "--ipc=host"
+                    "--net=host"
+                    "--runtime=nvidia"
+                    "--gpus=all"
+                    "-v" ".:/workspaces/lwlab"
+                    "-v" "$DATASETS_HOST_MOUNT_DIRECTORY:/datasets"
+                    "-v" "$MODELS_HOST_MOUNT_DIRECTORY:/models"
+                    "-v" "$EVAL_HOST_MOUNT_DIRECTORY:/eval"
+                    "-v" "$HOME/.bash_history:/home/$(id -un)/.bash_history"
+                    "-v" "$HOME/.config/osmo:/home/$(id -un)/.config/osmo"
+                    "-v" "/tmp:/tmp"
+                    "-v" "/tmp/.X11-unix:/tmp/.X11-unix:rw"
+                    "-v" "/var/run/docker.sock:/var/run/docker.sock"
+                    "-v" "$HOME/.Xauthority:/root/.Xauthority"
+                    "--env" "DISPLAY"
+                    "--env" "ACCEPT_EULA=Y"
+                    "--env" "PRIVACY_CONSENT=Y"
+                    "--env" "DOCKER_RUN_USER_ID=$(id -u)"
+                    "--env" "DOCKER_RUN_USER_NAME=$(id -un)"
+                    "--env" "DOCKER_RUN_GROUP_ID=$(id -g)"
+                    "--env" "DOCKER_RUN_GROUP_NAME=$(id -gn)"
+                    "--env" "OMNI_USER=\$omni-api-token"
+                    "--env" "OMNI_PASS=$OMNI_PASS"
+                    # Setting envs for XR: https://isaac-sim.github.io/IsaacLab/v2.1.0/source/how-to/cloudxr_teleoperation.html#run-isaac-lab-with-the-cloudxr-runtime
+                    # "--env" "XDG_RUNTIME_DIR=/workspaces/IsaacLab-Arena/submodules/IsaacLab/openxr/run"
+                    # "--env" "XR_RUNTIME_JSON=/workspaces/IsaacLab-Arena/submodules/IsaacLab/openxr/share/openxr/1/openxr_cloudxr.json"
+                    "--entrypoint" "/workspaces/lwlab/docker/run_docker.sh"
+                    )
+
+    # Allow X11 connections
+    xhost +local:docker
+
+    docker run "${DOCKER_RUN_ARGS[@]}" --interactive --rm --tty ${DOCKER_IMAGE_NAME}
+fi
---
+++ notebooks/isaac_arena_kitchen.py
@@ -0,0 +1,192 @@
+
+#%% Isaac Sim - Start up.
+
+print("Step 1 - Starting up Isaac Sim")
+
+import argparse
+
+import torch
+
+import pinocchio
+
+from isaaclab.app import AppLauncher
+
+parser = argparse.ArgumentParser(description="Isaac Arena CLI parser.")
+AppLauncher.add_app_launcher_args(parser)
+app_launcher_args = parser.parse_args([])
+
+app_launcher = AppLauncher(app_launcher_args)
+simulation_app = app_launcher.app
+
+
+#%% Lightwheel Labs - Load kitchen USDs and layout
+
+print("Step 2 - LightWheel Labs - Load kitchen USDs and layout")
+
+import gymnasium as gym
+
+from isaaclab_tasks.utils import import_packages
+from isaaclab.utils.configclass import configclass
+
+from lwlab.utils.env import ExecuteMode
+from lwlab.utils.env import load_robocasa_cfg_cls_from_registry
+import lwlab.utils.math_utils.transform_utils.numpy_impl as Tn
+
+# Load the environment configs into gym
+import_packages(
+    "lwlab.core",
+    # The blacklist is used to prevent importing configs from sub-packages
+    blacklist_pkgs=["utils", ".mdp", ".devices"]
+)
+import_packages("tasks")
+
+# Get the Kitchen Config from the registry
+layout = "robocasakitchen-4-2"
+scene_type = layout.split("-", 1)[0]
+layout_cfg = load_robocasa_cfg_cls_from_registry("scene", scene_type.capitalize(), "env_cfg_entry_point")
+
+# Add all the missing attributes to the config
+layout_cfg.scene_name = layout
+layout_cfg.device = "cuda"
+layout_cfg.num_envs = 1
+layout_cfg.usd_simplify = False
+layout_cfg.first_person_view = None
+layout_cfg.max_scene_retry = 10
+layout_cfg.max_object_placement_retry = 100
+
+# Get a task and robot
+task_name = "OpenDishwasher"
+robot_name = "PandaOmron-Rel"
+task_env_cfg = load_robocasa_cfg_cls_from_registry("task", task_name, "env_cfg_entry_point")
+robot_env_cfg = load_robocasa_cfg_cls_from_registry("robot", robot_name, "env_cfg_entry_point")
+
+@configclass
+class RobocasaEnvCfg(robot_env_cfg, task_env_cfg, layout_cfg):
+    pass
+
+env_cfg = RobocasaEnvCfg(
+    execute_mode=ExecuteMode.TELEOP,
+    usd_path=layout,
+    robot_scale=1.0,
+)
+
+from isaaclab.scene import InteractiveSceneCfg
+# CAUTION: Hacks on hacks!
+# Undo lwlabs monkey patch in: /workspaces/lwlab/lwlab/core/scenes/base.py
+if '_usd_path' in InteractiveSceneCfg.__dataclass_fields__:
+    del InteractiveSceneCfg.__dataclass_fields__['_usd_path']
+
+
+#%%
+
+print("Step 3 - Isaac Arena - Compile the environment")
+
+from isaaclab.scene import InteractiveSceneCfg
+from isaaclab.devices.teleop_device_factory import create_teleop_device
+
+from isaac_arena.assets.asset_registry import AssetRegistry
+from isaac_arena.assets.object import Object
+from isaac_arena.assets.background import Background
+from isaac_arena.utils.pose import Pose
+from isaac_arena.scene.scene import Scene
+from isaac_arena.embodiments.franka.franka import FrankaEmbodiment
+from isaac_arena.environments.isaac_arena_environment import IsaacArenaEnvironment
+from isaac_arena.environments.compile_env import ArenaEnvBuilder
+from isaac_arena.cli.isaac_arena_cli import get_isaac_arena_cli_parser
+from isaac_arena.tasks.open_door_task import OpenDoorTask
+from isaac_arena.assets.object_reference import OpenableObjectReference
+from isaac_arena.teleop_devices.spacemouse import SpacemouseTeleopDevice
+
+
+# Wrap the background
+kitchen_background = Background(
+    name="kitchen",
+    usd_path=env_cfg.usd_path,
+    object_min_z=0.1,
+)
+
+# Collect all the (distractor) objects
+objects: list[Object] = []
+for position_xyz, rotation_xyzw, obj in env_cfg.object_placements.values():
+    rotation_wxyz = Tn.convert_quat(rotation_xyzw, to="wxyz")
+    arena_object = Object(
+        name=obj.task_name,
+        prim_path="{ENV_REGEX_NS}/" + f"{obj.task_name}",
+        usd_path=obj.obj_path,
+        object_type=None,
+        initial_pose=Pose(
+            position_xyz=position_xyz,
+            rotation_wxyz=rotation_wxyz,
+        )
+    )
+    objects.append(arena_object)
+
+# Create a reference to the task-relavent object (the dishwasher)
+dishwasher = OpenableObjectReference(
+    name="dishwasher",
+    prim_path="{ENV_REGEX_NS}/kitchen/dishwasher_left_group",
+    parent_asset=kitchen_background,
+    openable_joint_name = "door_joint",
+)
+
+# Compose the scene
+scene = Scene([kitchen_background, *objects, dishwasher])
+
+# Create the task
+task = OpenDoorTask(dishwasher, openness_threshold=0.95, reset_openness=0.45)
+
+# Get the robot
+embodiment = FrankaEmbodiment(
+    # In front of the dishwasher
+    initial_pose=Pose(
+        position_xyz=(1.0, -3.0, 0.0),
+        rotation_wxyz=(0.0, 0.0, 0.0, 1.0),
+    )
+)
+
+# Teleop
+# spacemouse = SpacemouseTeleopDevice(pos_sensitivity=0.25, rot_sensitivity=0.25)
+
+isaac_arena_environment = IsaacArenaEnvironment(
+    name="lwlab_kitchen",
+    embodiment=embodiment,
+    scene=scene,
+    task=task,
+    # teleop_device=spacemouse,
+)
+
+args_parser = get_isaac_arena_cli_parser()
+args_cli = args_parser.parse_args([])
+
+builder = ArenaEnvBuilder(isaac_arena_environment, args_cli)
+env, cfg = builder.make_registered_and_return_cfg()
+env.reset()
+
+
+print("Trying to start teleop")
+teleop_interface = None
+try:
+    teleop_interface = create_teleop_device("spacemouse", cfg.teleop_devices.devices)
+    teleop_interface.reset()
+    print("Teleop started successfully.")
+except OSError as e:
+    print(f"Couldn't find teleop device: {e}")
+    print("Continuing without teleop device.")
+
+#%%
+
+print("Step 4 - Isaac Arena - Run the environment")
+
+import tqdm
+
+NUM_STEPS = 100 if teleop_interface is None else 1000000
+step = 0
+for _ in tqdm.tqdm(range(NUM_STEPS)):
+    with torch.inference_mode():
+        if teleop_interface is not None:
+            action = teleop_interface.advance()
+            actions = action.repeat(env.num_envs, 1)
+        else:
+            actions = torch.zeros(env.action_space.shape, device=env.unwrapped.device)
+        env.step(actions)
+    step += 1
